0368d2c0d627a9abb52e5af41548aaf2af776a38
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-07-10 12:26:07 -0500

    Fixed style using flake8

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -21,7 +21,9 @@ pmt = 639.81
 
 payment = (principal*(((1+interest/200)**(1/6)-1)) /
            (1-(((1+interest/200)**(1/6)))**-(years*12)))
-print(round(payment,2))
+print(round(payment, 2))
 
-number_payments = (((math.log(1 - principal * (((1 + interest/200)**(1/6) - 1)) / pmt)) / (-1 * math.log(((1+interest/200)**(1/6))))))
-print(round(number_payments,0))
+number_payments = (((math.log(1 - principal *
+                   (((1 + interest/200)**(1/6) - 1)) / pmt)) /
+                   (-1 * math.log(((1+interest/200)**(1/6))))))
+print(round(number_payments, 0))
