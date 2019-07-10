c48dd6e7ea6ed4c70f0c66d5070492fd27c7af6a
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-07-10 12:36:41 -0500

    Improved printing of number of payments to make code block

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -29,4 +29,7 @@ print(f'Principal:               {principal} CAD\n'
 number_payments = (((math.log(1 - principal *
                    (((1 + interest/200)**(1/6) - 1)) / pmt)) /
                    (-1 * math.log(((1+interest/200)**(1/6))))))
-print(round(number_payments, 0))
+print(f'Principal:               {principal} CAD\n'
+      f'Interest:                {interest} %\n'
+      f'Monthly payment made:    {pmt} CAD\n'
+      f'Number of payments:      {round(number_payments, 0)}')
