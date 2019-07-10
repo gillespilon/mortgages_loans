bd1992c7df6bcb446f72a88357b177017a9b0c09
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-07-10 14:05:13 -0500

    Converted principal to an input

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -13,7 +13,7 @@ time -f '%e' ./mortgage_calculator.py > mortgage_calculator.txt
 import math
 
 
-principal = 100000
+principal = float(input('What is the principal amount? '))
 interest = 6
 years = 25
 pmt = 639.81
@@ -25,6 +25,7 @@ print(f'Principal:               {principal} CAD\n'
       f'Interest:                {interest} %\n'
       f'Amortization:            {years} years\n'
       f'Monthly payment to make: {round(payment, 2)} CAD\n')
+
 
 number_payments = (((math.log(1 - principal *
                    (((1 + interest/200)**(1/6) - 1)) / pmt)) /
