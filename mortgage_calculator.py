67f9ebd9926db3326190aee45594028142a12163
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-07-11 12:02:23 -0500

    Improved output formatting using f-strings

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -24,25 +24,27 @@ import math
 principal = float(input('What is the principal amount?           '))
 interest = float(input('What is the interest rate (%)?          '))
 years = float(input('What is the amortization period (year)? '))
-pmt = float(input('What monthly payment would you make? '))
+pmt = float(input('What monthly payment would you make?    '))
 
 
 payment = (principal*(((1+interest/200)**(1/6)-1)) /
            (1-(((1+interest/200)**(1/6)))**-(years*12)))
 accrued_interest = payment * 12 * years - principal
-print(f'\nPrincipal:               {principal} CAD\n'
-      f'Interest:                {interest} %\n'
-      f'Amortization:            {years} year\n'
-      f'Monthly payment to make: {round(payment, 2)} CAD\n'
-      f'Accrued interest:        {round(accrued_interest, 2)}\n')
+print(f'\nPrincipal:               {principal:<10,.2f} CAD'.replace(',', ' '),
+      f'\n'
+      f'Interest:                {interest:10.2f} %\n'
+      f'Amortization:            {years:7.0f}    years\n'
+      f'Monthly payment to make: {payment:10.2f} CAD\n'
+      f'Accrued interest:        {accrued_interest:10.2f} CAD\n')
 
 
 number_payments = (((math.log(1 - principal *
                    (((1 + interest/200)**(1/6) - 1)) / pmt)) /
                    (-1 * math.log(((1+interest/200)**(1/6))))))
 accrued_interest_two = pmt * number_payments - principal
-print(f'Principal:               {principal} CAD\n'
-      f'Interest:                {interest} %\n'
-      f'Monthly payment made:    {pmt} CAD\n'
-      f'Number of payments:      {round(number_payments, 0)}\n'
-      f'Accrued interest:        {round(accrued_interest_two, 2)}\n')
+print(f'\nPrincipal:               {principal:<10,.2f} CAD'.replace(',', ' '),
+      f'\n'
+      f'Interest:                {interest:10.2f} %\n'
+      f'Monthly payment made:    {pmt:10.2f} CAD\n'
+      f'Number of payments:      {number_payments:7.0f}\n'
+      f'Accrued interest:        {accrued_interest_two:10.2f} CAD\n')
