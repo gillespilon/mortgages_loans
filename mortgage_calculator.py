08f49a66b9e3a5c30e808dbc6cc9a196d7ef6d92
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-07-11 12:26:50 -0500

    Converted two main code blocks to functions

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -21,30 +21,38 @@ Payment = Principal x  ------------------------
 import math
 
 
+def calcpayment(principal, interest, years, pmt):
+    payment = (principal*(((1+interest/200)**(1/6)-1)) /
+               (1-(((1+interest/200)**(1/6)))**-(years*12)))
+    accrued_interest = payment * 12 * years - principal
+    print(f'\nPrincipal:               {principal:<10,.2f} CAD'
+          .replace(',', ' '),
+          f'\n'
+          f'Interest:                {interest:10.2f} %\n'
+          f'Amortization:            {years:7.0f}    years\n'
+          f'Monthly payment to make: {payment:10.2f} CAD\n'
+          f'Accrued interest:        {accrued_interest:10.2f} CAD\n')
+
+
+def calcnumpayments(principal, interest, pmt):
+    number_payments = (((math.log(1 - principal *
+                       (((1 + interest/200)**(1/6) - 1)) / pmt)) /
+                       (-1 * math.log(((1+interest/200)**(1/6))))))
+    accrued_interest_two = pmt * number_payments - principal
+    print(f'\nPrincipal:               {principal:<10,.2f} CAD'
+          .replace(',', ' '),
+          f'\n'
+          f'Interest:                {interest:10.2f} %\n'
+          f'Monthly payment made:    {pmt:10.2f} CAD\n'
+          f'Number of payments:      {number_payments:7.0f}\n'
+          f'Accrued interest:        {accrued_interest_two:10.2f} CAD\n')
+
+
 principal = float(input('What is the principal amount?           '))
 interest = float(input('What is the interest rate (%)?          '))
 years = float(input('What is the amortization period (year)? '))
 pmt = float(input('What monthly payment would you make?    '))
 
 
-payment = (principal*(((1+interest/200)**(1/6)-1)) /
-           (1-(((1+interest/200)**(1/6)))**-(years*12)))
-accrued_interest = payment * 12 * years - principal
-print(f'\nPrincipal:               {principal:<10,.2f} CAD'.replace(',', ' '),
-      f'\n'
-      f'Interest:                {interest:10.2f} %\n'
-      f'Amortization:            {years:7.0f}    years\n'
-      f'Monthly payment to make: {payment:10.2f} CAD\n'
-      f'Accrued interest:        {accrued_interest:10.2f} CAD\n')
-
-
-number_payments = (((math.log(1 - principal *
-                   (((1 + interest/200)**(1/6) - 1)) / pmt)) /
-                   (-1 * math.log(((1+interest/200)**(1/6))))))
-accrued_interest_two = pmt * number_payments - principal
-print(f'\nPrincipal:               {principal:<10,.2f} CAD'.replace(',', ' '),
-      f'\n'
-      f'Interest:                {interest:10.2f} %\n'
-      f'Monthly payment made:    {pmt:10.2f} CAD\n'
-      f'Number of payments:      {number_payments:7.0f}\n'
-      f'Accrued interest:        {accrued_interest_two:10.2f} CAD\n')
+calcpayment(principal, interest, years, pmt)
+calcnumpayments(principal, interest, pmt)
